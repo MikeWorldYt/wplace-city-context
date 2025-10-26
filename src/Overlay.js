57ca241cd9e61f963a644cbbc9c63579e1728069
mikeworldyt <diazmig98@gmail.com>
2025-10-26 00:05:19 -0600

    feat: credentials input interface and logic

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,10 +1,33 @@
 export function initOverlay() {
   const panel = document.createElement('div');
   panel.id = 'cc-wplace-panel';
+
   panel.innerHTML = `
-    <div><b>Wplace City Context</b></div>
-    <input type="text" id="credential" placeholder="Enter credential" />
-    <button id="run">RUN</button>
+    <div style="text-align: center; "><b>Wplace City Context</b></div>
+    <div id="cc-win-credential">
+      <p>Credential:</p>
+      <div class="input-wrapper">
+        <input type="password" id="credential" placeholder="Enter credential" />
+        <img id="toggleEye" 
+          src="https://raw.githubusercontent.com/MikeWorldYt/wplace-city-context/main/src/assets/eye-closed.svg"
+          style="position: absolute; right: 8px; top: 50%; transform: translateY(-50%); width: 20px; height: 20px; cursor: pointer;">
+      </div>
+    </div>
+    <button id="send-credential">
+      Send
+    </button>
   `;
+
   document.body.appendChild(panel);
+
+  const input = panel.querySelector('#credential');
+  const eye = panel.querySelector('#toggleEye');
+
+  eye.addEventListener('click', () => {
+    const isHidden = input.type === 'password';
+    input.type = isHidden ? 'text' : 'password';
+    eye.src = isHidden
+      ? 'https://raw.githubusercontent.com/MikeWorldYt/wplace-city-context/main/src/assets/eye-open.svg'
+      : 'https://raw.githubusercontent.com/MikeWorldYt/wplace-city-context/main/src/assets/eye-closed.svg';
+  });
 }

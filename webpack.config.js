043ca0e223cc63b665e338ac6995d0a5fbaac57e
mikeworldyt <diazmig98@gmail.com>
2025-10-25 18:29:11 -0600

    feat: Webpack initial config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+const TampermonkeyHeader = `
+  // ==UserScript==
+  // @name         Wplace City Context Overlay
+  // @namespace    https://github.com/MikeWorldYt/wplace-city-context
+  // @version      0.1
+  // @description  Overlay visual para Wplace (zonas y nodos)
+  // @author       MikeWorldYt
+  // @match        https://wplace.live/*
+  // @grant        GM_addStyle
+  // ==/UserScript==
+`;
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'WplaceCityContext.user.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: TampermonkeyHeader,
+      raw: true,
+    }),
+  ],
+  mode: 'production',
+};
